01e49443d9262f4e1930eff5efef1860efce3827
reddigari <samirreddigari@gmail.com>
2017-10-31 13:27:37 -0400

    2017 Leagues: update scripts and data after week 8

---
+++ BTownsFinest/update_league.py
@@ -0,0 +1,13 @@
+import sys
+from espnffl import FFLeague
+
+week = int(sys.argv[1])
+
+lg = FFLeague('BTownsFinest', 1083362)
+if "--proj" in sys.argv:
+    lg.get_proj(week)
+lg.get_scores(week)
+lg.get_rosters_and_matchup(week)
+lg.output_week_vis_json(8)
+if "--team-ave" in sys.argv:
+    lg.output_team_vs_average_json(week)
