52667d010fe69d4df4fa69e777664f6c6ac7a614
reddigari <samirreddigari@gmail.com>
2017-11-06 18:21:52 -0500

    2017 Leagues: data through week 9 Sunday

--- BTownsFinest/update_league.py
+++ BTownsFinest/update_league.py
@@ -8,6 +8,6 @@ if "--proj" in sys.argv:
     lg.get_proj(week)
 lg.get_scores(week)
 lg.get_rosters_and_matchup(week)
-lg.output_week_vis_json(8)
+lg.output_week_vis_json(week)
 if "--team-ave" in sys.argv:
     lg.output_team_vs_average_json(week)
